d28708351d9219e43a395411e212e52c61445796
Farras Rafi Permana <farras.rafi@ui.ac.id>
2023-05-28 23:15:23 +0700

    Spring Boot Initial Project

---
+++ Backend/WebPassport/src/main/java/com/WebPassport/WebPassportApplication.java
@@ -0,0 +1,13 @@
+package com.WebPassport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPassportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPassportApplication.class, args);
+	}
+
+}
---
+++ Backend/WebPassport/src/test/java/com/WebPassport/WebPassportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebPassport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPassportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
